f95c1eac00fab343a8adee9d9887f39e029a8e1d
lars0308 <53699011+lars0308@users.noreply.github.com>
2026-08-10 00:36:28 +0200

    Load distinct workflow mode controller

--- admin-console.js
+++ admin-console.js
@@ -1,5 +1,5 @@
 (()=>{
   'use strict';
   const load=(src,onload)=>{const script=document.createElement('script');script.src=src;script.async=false;if(onload)script.addEventListener('load',onload,{once:true});document.head.appendChild(script);return script};
-  load('./admin-console-core.js?v=20260810-1',()=>load('./ui-regression-fixes.js?v=20260810-1'));
+  load('./admin-console-core.js?v=20260810-2',()=>load('./ui-regression-fixes.js?v=20260810-2',()=>load('./mode-flow-ui.js?v=20260810-1')));
 })();
